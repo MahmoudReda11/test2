26f0cf71395398754627265ed6ed061c9bb18ad5
Mahmoud Reda <115508120+MahmoudReda11@users.noreply.github.com>
2024-05-09 01:23:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -35,4 +35,4 @@ patharr.splice(-1);
 let path = patharr.join("/");
 let host = window.location.protocol + "//" + window.location.host + path;
 let redirectValue = document.getElementById("redDir").value;
-document.getElementById("redDir").value = `${host}/thanksPage.html`;
+document.getElementById("redDir").value = `${host}/thanks.html`;
